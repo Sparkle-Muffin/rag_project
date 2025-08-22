acfc07fd5cbcf5963fa7a9d5f227426eddb5e980
pervitin <slawomirkleszczewski@gmail.com>
2025-08-22 17:47:32 +0200

    add streamlit chat

---
+++ common/streamlit_chat.py
@@ -0,0 +1,40 @@
+# from openai import OpenAI
+import streamlit as st
+import time
+import pandas as pd
+import numpy as np
+
+
+st.title("RAG Project")
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+_LOREM_IPSUM = """
+Lorem ipsum dolor sit amet, **consectetur adipiscing** elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+"""
+
+
+def stream_data():
+    for word in _LOREM_IPSUM.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+
+if prompt := st.chat_input("Zadaj pytanie:"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = stream_data
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
