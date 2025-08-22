12dcdd6fee31cea65e85d60609700ea3279df991
pervitin <slawomirkleszczewski@gmail.com>
2025-08-22 21:22:43 +0200

    Bielik working in bash

--- common/streamlit_chat.py
+++ common/streamlit_chat.py
@@ -1,10 +1,6 @@
 # from openai import OpenAI
 import streamlit as st
-import time
-from bielik_api import run_bielik
-
-
-run_bielik()
+from bielik_api import call_model
 
 
 st.title("RAG Project")
@@ -18,24 +14,11 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 
-_LOREM_IPSUM = """
-Lorem ipsum dolor sit amet, **consectetur adipiscing** elit, sed do eiusmod tempor
-incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-"""
-
-
-def stream_data():
-    for word in _LOREM_IPSUM.split(" "):
-        yield word + " "
-        time.sleep(0.02)
-
-
 if prompt := st.chat_input("Zadaj pytanie:"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        response = st.write_stream(stream_data)
+        response = st.write_stream(call_model(prompt))
     st.session_state.messages.append({"role": "assistant", "content": response})
